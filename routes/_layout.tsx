1e1c369ac8d0c052085368285089b873e1d04d01
Burawi <burawi.ba@gmail.com>
2024-03-01 21:00:24 +0300

    chore: switch footer message

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -318,7 +318,7 @@ export default function Layout({ state, Component }: PageProps) {
 
           <div>
             <p class="text-center text-gray-500 dark:text-gray-400">
-              Made with ❤️ by a Muslim person
+              Made with ❤️ by a group of Muslims
             </p>
           </div>
         </div>
