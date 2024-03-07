0b9336f79bd2b3392737e37bc0de0a69cabbbf1b
shiraz27 <hosniishiraz@gmail.com>
2024-03-07 11:52:05 +0100

    hide submit

--- islands/navigation.tsx
+++ islands/navigation.tsx
@@ -50,7 +50,7 @@ export const Navigation = ({ state }) => {
                       class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
                       placeholder="Search"
                     />
-                    <input type="submit" />
+                    <input hidden type="submit" />
                   </div>
                 </div>
                 <CountryDropdown state={state} />
@@ -151,7 +151,7 @@ export const Navigation = ({ state }) => {
                     class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
                     placeholder="Search"
                   />
-                  <input type="submit" />
+                  <input hidden type="submit" />
                 </div>
               </div>
             </div>
