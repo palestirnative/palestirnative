dbab73fce335c797bd8e3c1071d617595adf3cf5
shiraz27 <hosniishiraz@gmail.com>
2024-03-08 16:34:03 +0100

    fix: wrong button title

--- islands/suggest-boycott.tsx
+++ islands/suggest-boycott.tsx
@@ -43,7 +43,7 @@ export default function SuggestAlternative({ boycotts, state, alternative }) {
         onClick={showModal}
         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
-        {state.locale["Suggest an alternative"]}
+        {state.locale["Suggest a boycott"]}
       </button>
       <div
         hidden={!shouldShowModal}
