4c2ed50a87c01628e5a82e245c6826921a8b8201
Daly <dalidaliovic@gmail.com>
2024-03-12 12:24:39 +0100

    chore: removed obsolete onblur logic

--- islands/form/tag-input.tsx
+++ islands/form/tag-input.tsx
@@ -70,10 +70,6 @@ export default function TagInput<T extends HasLabel>(
     setIsFocused(true);
   };
 
-  const handleInputBlur = () => {
-    !shouldShowOptions && setIsFocused(false);
-  };
-
   const handleMouseLeave = () => {
     setIsFocused(false);
     inputRef.current?.blur();
@@ -107,7 +103,6 @@ export default function TagInput<T extends HasLabel>(
             autocomplete="off"
             onKeyUp={handleInputChange}
             onFocus={handleInputFocus}
-            onBlur={handleInputBlur}
             ref={inputRef}
             class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
           />
